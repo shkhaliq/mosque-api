6838f0f227a3fabded1cc5d43b503103648a95dc
Haris Khaliq <haris.khaliq@thescore.com>
2019-02-07 00:53:00 -0500

    Make salah names unique

--- app/models/salah.rb
+++ app/models/salah.rb
@@ -1,7 +1,7 @@
 class Salah < ApplicationRecord
   belongs_to :mosque
   # validation
-  validates_presence_of :name
+  validates_presence_of :name, :iqamah
+  validates_uniqueness_of :name
 
-  validates_presence_of :iqamah
 end
--- spec/factories/salahs.rb
+++ spec/factories/salahs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :salah do
-    name { Faker::StarWars.character }
+    name { Faker::Name.unique.name }
     begins { Faker::StarWars.character }
     iqamah { Faker::Date.between(2.days.ago, Date.today) }
     mosque { nil }
